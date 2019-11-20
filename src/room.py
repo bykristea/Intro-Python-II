cce3de33923663cee719274ac453cca42563a1d1
Kristea Gamel <krgamel@gmail.com>
2019-11-19 20:18:17 -0600

    fixed typo in room and added  return

--- src/room.py
+++ src/room.py
@@ -1,7 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 
+
 class Room:
     def __init__(self, name, description):
-        self.name = description
+        self.name = name
         self.descritpion = description
+
+        def __str__(self):
+            return f"{self.description}"
