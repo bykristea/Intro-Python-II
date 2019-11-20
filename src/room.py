d93e771d864647cba6ab35f8a5d733be8f1261c6
Kristea Gamel <krgamel@gmail.com>
2019-11-19 20:32:03 -0600

    redoing to track on my branch

--- src/room.py
+++ src/room.py
@@ -1,11 +1,2 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
-
-
-class Room:
-    def __init__(self, name, description):
-        self.name = name
-        self.descritpion = description
-
-        def __str__(self):
-            return f"{self.description}"
